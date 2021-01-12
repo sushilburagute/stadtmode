ae2190058a4c0325ef11a196a7140ab1fc17b23a
sushilburagute <codetastic101@gmail.com>
2021-01-12 12:12:42 +0530

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl">Hello!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
